909744a053b2d0efceb549e7c19841d9017073a8
Julie <Julia@MacBook-Pro-Julie.local>
2021-11-12 12:31:16 +0300

    few fixes

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faAngleLeft, faAngleRight, faPause } from '@fortawesome/free-solid-svg-icons';
 
@@ -13,11 +13,10 @@ const Player = ({
   songs,
   setSongs
 }) => {
-  //Use Effect
-  useEffect(() => {
+  const activeLibraryHandler = (nextPrev) => {
     //add active state
     const newSongs = songs.map((song) => {
-      if (song.id === currentSong.id) {
+      if (song.id === nextPrev.id) {
         return {
           ...song,
           active: true
@@ -30,8 +29,7 @@ const Player = ({
       }
     });
     setSongs(newSongs);
-  }, [currentSong]);
-
+  };
   //Event Handlers
   const playSongHandler = () => {
     if (isPlaying) {
@@ -53,14 +51,17 @@ const Player = ({
     let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
     if (direction === 'skip-forward') {
       await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+      activeLibraryHandler(songs[(currentIndex + 1) % songs.length]);
     }
     if (direction === 'skip-back') {
       if ((currentIndex - 1) % songs.length === -1) {
         await setCurrentSong(songs[songs.length - 1]);
+        activeLibraryHandler(songs[songs.length - 1]);
         if (isPlaying) audioRef.current.play();
         return;
       }
       await setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+      activeLibraryHandler(songs[(currentIndex - 1) % songs.length]);
     }
     if (isPlaying) audioRef.current.play();
   };
